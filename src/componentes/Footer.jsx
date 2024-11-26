dc6ec656b127e26ca7f50d88ef7bcc833c51e759
PilarGallegoS <pgsubias@gmail.com>
2024-11-26 20:57:57 +0000

    centrar verticalmente

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-white text-center py-4">
+        <footer className="bg-dark text-white text-center p-4">
             <div className="container center-vertically">
-                <p> <small>Copyright &copy; Your Website {new Date().getFullYear()}</small></p> {/* Lo puse así para probar este de fecha. */}
+                <p className="m-0"> <small>Copyright &copy; Your Website {new Date().getFullYear()}</small></p> {/* Lo puse así para probar este de fecha. */}
             </div>
         </footer>
     );
